2515e823b0edc4f85149816304cbf676eece3d6a
Yudina Tamila <taaamilaaa1988@gmail.com>
2021-08-13 17:49:11 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-console.log('hello');
+const delay = ms => {
+
+};
+
+
+
+const logger = time => console.log(`Resolved after ${time}ms`);
+
+const reject = () => console.log('error');
+
+// promise.then(logger())
+
+//Напиши функцию delay(ms), которая возвращает промис, переходящий в 
+//состояние "resolved" через ms миллисекунд.Значением исполнившегося 
+//промиса должно быть то кол - во миллисекунд которое передали во время
+//вызова функции delay.
+
+
+
+
+delay(2000).then(logger); // Resolved after 2000ms
+// delay(1000).then(logger); // Resolved after 1000ms
+// delay(1500).then(logger); // Resolved after 1500ms
