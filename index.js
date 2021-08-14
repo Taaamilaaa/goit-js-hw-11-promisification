ca095714a4e92a8dd1edd20b9aef814b48173120
Yudina Tamila <taaamilaaa1988@gmail.com>
2021-08-14 13:20:53 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,49 @@
-const delay = ms => {
+// Задание 1
+//const delay = ms => {
+//     return new Promise((resolve, reject) => {
 
+//         setTimeout(() => {
+
+//             resolve(ms);
+
+//             reject("Error passed into reject function :(")
+//         }, ms);
+//     }
+//     )
+// };
+
+// const logger = time => console.log(`Resolved after ${time}ms`);
+
+// // Вызовы функции для проверки
+// delay(2000).then(logger);
+// delay(1000).then(logger);
+// delay(1500).then(logger);
+
+// Задание 2
+
+const users = [
+  { name: 'Mango', active: true },
+  { name: 'Poly', active: false },
+  { name: 'Ajax', active: true },
+  { name: 'Lux', active: false },
+];
+
+ const toggleUserState = (allUsers, userName) => {
+  return new Promise((resolve, reject) => {    
+      resolve(allUsers, userName);
+
+  });
 };
 
 
+const logger = (allUsers, userName) => {
+    const result = allUsers.map((user) => {
+        if (user.name === userName) {
+            user = { ...user, active: !user.active }
+        }
+    })
+                console.table(result);
 
-const logger = time => console.log(`Resolved after ${time}ms`);
-
-const reject = () => console.log('error');
-
-// promise.then(logger())
-
-//Напиши функцию delay(ms), которая возвращает промис, переходящий в 
-//состояние "resolved" через ms миллисекунд.Значением исполнившегося 
-//промиса должно быть то кол - во миллисекунд которое передали во время
-//вызова функции delay.
-
-
-
-
-delay(2000).then(logger); // Resolved after 2000ms
-// delay(1000).then(logger); // Resolved after 1000ms
-// delay(1500).then(logger); // Resolved after 1500ms
+}
+toggleUserState(users, 'Mango').then(logger);
+toggleUserState(users, 'Lux').then(logger);
